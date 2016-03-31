020375587fd6098afb6d9794ad177446066d8ec7
petteri1991 <petteri-anttila@hotmail.com>
2016-03-31 18:12:48 +0300

    Initial commit

---
+++ Tiled/Assets/Tiled2Unity/Scripts/PlayerControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float Speed = 5.0f;
+    Animator animator;
+    bool walk;
+    // Use this for initialization
+    void Start () {
+        animator = this.GetComponent<Animator>();
+	}
+    // Update is called once per frame
+    void Update () {
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            walk = true;
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            walk = true;
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            walk = true;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            walk = true;
+        }
+        else
+            walk = false;
+        animator.SetBool("walk", walk);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * Speed, 0.8f),Mathf.Lerp(0, Input.GetAxis("Vertical") * Speed, 0.8f));
+        
+    }
+}
